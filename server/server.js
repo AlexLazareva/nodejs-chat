0001b19c45aa1c0aa1bb19bda2c7e7c5bb45e3d3
Alex Lazareva <alex.lazareva25@yandex.ru>
2018-05-19 19:26:00 +0300

    create project

---
+++ server/server.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'index.html'));
+});
+
+app.listen(8000, '0.0.0.0', () => {
+    console.log('Server started on port 8000');
+});
