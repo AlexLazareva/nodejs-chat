ce6d3fa42905f936a0a84ecd14045bfe9a2ae98d
Alex Lazareva <alex.lazareva25@yandex.ru>
2018-05-20 15:34:41 +0300

    include autenticate passport-jwt

--- server/server.js
+++ server/server.js
@@ -8,6 +8,19 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server, {serveClient: true});
 const mongoose = require('mongoose');
 
+const passport = require('passport');
+const { Strategy, ExtractJwt } = require('passport-jwt');
+
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: '0NETjPmf2dxu'
+};
+
+passport.use(new Strategy(opts, function(jwt_payload, done) {
+    if(jwt_payload != void(0)) return done(false, jwt_payload);
+    done();
+}));
+
 mongoose.connect('mongodb://localhost:27017/chat', {});
 mongoose.Promise = require('bluebird');
 
@@ -20,11 +33,17 @@ require('./sockets')(io);
 
 app.use('/assets', express.static('./client/public'));
 
-app.get('/', (req, res) => {
-    res.render('index.html');
+function checkAuth(req, res, next) {
+    passport.authenticate('jwt', {session: false}, (err, decryptToken, jwtError) => {
+        if(jwtError != void(0) || err != void(0)) return res.render('index.html', {error: err || jwtError});
+        req.user = decryptToken;
+        next();
+    })(req, res, next);
+}
+
+app.get('/', checkAuth, (req, res) => {
+    res.render('index.html', {date: new Date()});
 });
-
-
 
 server.listen(8000, '0.0.0.0', () => {
     console.log('Server started on port 8000');
