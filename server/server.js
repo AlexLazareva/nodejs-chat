79bfc5d61a7a092a1a519bd6f711d179f7bf0fe5
Alex Lazareva <alex.lazareva25@yandex.ru>
2018-05-20 13:55:30 +0300

    create sockets

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server, {serveClient: true});
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/nodejs-chat', {});
+mongoose.connect('mongodb://localhost:27017/chat', {});
 mongoose.Promise = require('bluebird');
 
 nunjucks.configure('./client/views', {
@@ -16,28 +16,15 @@ nunjucks.configure('./client/views', {
     express: app
 });
 
+require('./sockets')(io);
+
 app.use('/assets', express.static('./client/public'));
 
 app.get('/', (req, res) => {
     res.render('index.html');
 });
 
-io.on('connection', function (socket) {
-    socket.emit('connected', "Socket connected!");
 
-    socket.join('all');
-
-    socket.on('msg', content => {
-        console.log("MSG", content);
-        const obj = {
-            date: new Date(),
-            content: content,
-            username: socket.id
-        };
-        socket.emit("message", obj);
-        socket.to('all').emit("message", obj);
-    });
-});
 
 server.listen(8000, '0.0.0.0', () => {
     console.log('Server started on port 8000');
