deb5d0be7d3065ddae19048254fd4cd3de26d5f1
Alex Lazareva <alex.lazareva25@yandex.ru>
2018-05-26 22:04:46 +0300

    add cookie-parser

--- server/config.js
+++ server/config.js
@@ -1,8 +1,15 @@
 "use strict";
 
+function ExtractJwt(req) {
+    let token  = null;
+    if(req.cookies && req.cookies.token != void(0)) token = req.cookies['token'];
+
+    return token;
+}
+
 module.exports = {
     jwt: {
-        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+        jwtFromRequest: ExtractJwt,
         secretOrKey: '0NETjPmf2dxu'
     },
     expiresIn: '1 day'
